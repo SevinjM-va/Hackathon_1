c9884c34999f3a93a483167b91c0dc9296fc12cb
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-09-23 12:16:06 -0700

    html and css

---
+++ BeBrave/script.js
@@ -0,0 +1,16 @@
+
+
+
+let language = document.getElementById('language');
+
+
+
+function selectLanguage (){
+  console.log('sdasd');
+//   if (option[0].value = en){
+//     alert('You choose english')
+//   } else {
+//     alert('you choose az')
+//   }
+}
+
