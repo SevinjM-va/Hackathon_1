06d07deab578433859e4e0832bdb484651d93dc0
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-09-24 06:54:56 -0700

    task

--- BeBrave/script.js
+++ BeBrave/script.js
@@ -1,16 +1,39 @@
 
+let title = document.getElementById('title');
+console.log(title);
+
+let label1 =  document.getElementById('nameLabel');
+let inputName = document.getElementsByClassName('inputField')[0];
+console.log(inputName);
+let inputEmail = document.getElementsByClassName('inputField')[1];
+let inputPassword = document.getElementsByClassName('inputField')[3];
 
 
-let language = document.getElementById('language');
 
 
+let loginbtn = document.querySelector('.login');
+let signupbtn = document.querySelector('.singup');
+loginbtn.addEventListener('click',login);
+signupbtn.addEventListener('click',signUp);
 
-function selectLanguage (){
-  console.log('sdasd');
-//   if (option[0].value = en){
-//     alert('You choose english')
-//   } else {
-//     alert('you choose az')
-//   }
+
+function login(){
+  loginbtn.style.backgroundColor = 'blue';
+  title.textContent = 'Log in';
+  inputName.style.maxHeight = 0;
+  inputName.style.border = 0;
+  label1.textContent = '';
+}
+function signUp(e){
+  signupbtn.style.backgroundColor = 'blue';
+  title.textContent = 'Sign up';
+  inputName.style.maxHeight = '65px';
+  inputName.style.border = '2px solid gray';
+  label1.textContent = 'Name';
+
+  if (inputName.value && inputEmail.value && inputPassword){
+    alert( `CONGRATULATIONS..YOU SIGN UP SUCCESSFULLY!! `);
+    return
+  }
 }
 
