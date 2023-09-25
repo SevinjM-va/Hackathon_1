5b6d77b562e97606e5b8892a285c892b6e355240
Sevinj90r <Mammadovasevindj90@gmail.com>
2023-09-24 20:54:51 -0700

    html css and js

--- BeBrave/script.js
+++ BeBrave/script.js
@@ -1,39 +1,48 @@
 
 let title = document.getElementById('title');
-console.log(title);
+
 
 let label1 =  document.getElementById('nameLabel');
 let inputName = document.getElementsByClassName('inputField')[0];
-console.log(inputName);
 let inputEmail = document.getElementsByClassName('inputField')[1];
-let inputPassword = document.getElementsByClassName('inputField')[3];
+let inputPassword = document.getElementsByClassName('inputField')[2];
+
+let menubox = document.getElementById('menubox');
+
+let ul = document.querySelector('ul');
+let loginsign = document.querySelector('.login-sign');
 
 
+function secretcheckbox(){
+ ul.classList.toggle('mystyle');
+ loginsign.classList.toggle('mystyle');
 
+ let loginbtn = document.getElementsByTagName('button');
+ loginbtn[0].addEventListener('click',console.log('ass'));
 
-let loginbtn = document.querySelector('.login');
-let signupbtn = document.querySelector('.singup');
-loginbtn.addEventListener('click',login);
-signupbtn.addEventListener('click',signUp);
+ let signbtn = document.querySelector('.signup');
+ signbtn.addEventListener('click',signupFunc);
 
-
-function login(){
-  loginbtn.style.backgroundColor = 'blue';
-  title.textContent = 'Log in';
-  inputName.style.maxHeight = 0;
-  inputName.style.border = 0;
-  label1.textContent = '';
-}
-function signUp(e){
-  signupbtn.style.backgroundColor = 'blue';
-  title.textContent = 'Sign up';
-  inputName.style.maxHeight = '65px';
-  inputName.style.border = '2px solid gray';
-  label1.textContent = 'Name';
-
-  if (inputName.value && inputEmail.value && inputPassword){
-    alert( `CONGRATULATIONS..YOU SIGN UP SUCCESSFULLY!! `);
-    return
-  }
+function loginFunc(){
+  alert('xzxzxz');
 }
 
+}
+
+
+
+// function login(){
+  
+//   loginbtn.style.backgroundColor = 'blue';
+  // title.textContent = 'Log in';
+  // inputName.style.maxHeight = 0;
+  // inputName.style.border = 0;
+  // label1.textContent = '';
+// }
+// function signUp(){
+//   signupbtn.style.backgroundColor = 'blue';
+//   title.textContent = 'Sign up';
+//   inputName.style.maxHeight = '65px';
+//   inputName.style.border = '2px solid gray';
+//   label1.textContent = 'Name';
+// }
